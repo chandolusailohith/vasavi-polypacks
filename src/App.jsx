1255052db4411f690bd3118ffbbae28c7d7f1e21
Sai Lohith <your.email@gmail.com>
2025-12-26 12:48:39 +0530

    Scroll to top on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/layout/Navbar.jsx";
 import Footer from "./components/layout/Footer.jsx";
 import Hero from "./components/sections/Hero.jsx";
@@ -8,6 +8,11 @@ import WhyUs from "./components/sections/WhyUs.jsx";
 import Contact from "./components/sections/Contact.jsx";
 
 const App = () => {
+  useEffect(() => {
+    // whenever app loads (including refresh), go to top
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="site-root">
       <Navbar />
